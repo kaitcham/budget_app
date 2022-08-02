92283bc0ab9371af8e9ff088a916519f593f5151
Munezero Christian <chamsf5@aol.com>
2022-08-02 18:51:01 +0200

    Allow users to create and display categories

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,39 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: %i[show destroy]
+
+  def index
+    @categories = Category.all
+  end
+
+  def show; end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    @category.author_id = current_user.id
+
+    if @category.save
+      redirect_to categories_url, notice: 'Category was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to categories_url, notice: 'Category was successfully destroyed.'
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
+end
