76e56b4879874707e1518702831b70c27ff272f3
Munezero Christian <chamsf5@aol.com>
2022-08-03 11:07:51 +0200

    Add validations to models

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,10 @@ class Category < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :expenses
 
+  validates_presence_of :author
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :icon, presence: true
+
   def total
     sum = 0
     expenses.each do |exp|
