e5ceda52f50edbbb3f020e8e489170a33215c662
Munezero Christian <chamsf5@aol.com>
2022-08-03 11:37:49 +0200

    Add dependent destroy

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
-  has_many :expenses
+  has_many :expenses, dependent: :destroy
 
   validates_presence_of :author
   validates :name, presence: true, length: { maximum: 30 }
